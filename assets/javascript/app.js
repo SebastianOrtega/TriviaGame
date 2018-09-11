d3ca78932d85412046e1a50ba986dccb3360e49a
SebastianOrtega <sortega@moviltrack.com>
2018-09-11 14:49:57 -0500

    some cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,3 @@
-
-
-let question0 = {
-    question: "Which year Die Hard was premier?",
-    options: ["1986", "1987", "1988", "1989"],
-    correct: "1988",
-    gif: "https://media.giphy.com/media/AphxZxXiVklYQ/giphy.gif"
-}
-let question1 = {
-    question: "Who was the director of Leathal Weapon?",
-    options: ["Mel Gibson", "Danny Glover", "Richard Donner", "Traci Wolfe"],
-    correct: "Richard Donner",
-    gif: "https://media.giphy.com/media/3b1JW7LxfsAKs/giphy.gif"
-}
-let question2 = {
-    question: "In Point break who is Johnny Utah?",
-    options: ["Gary Busey", "Patrick Swayze", "John C. McGinley", "Keanu Reeves"],
-    correct: "Keanu Reeves",
-    gif: "https://media.giphy.com/media/aDYXQy3W8XFG8/giphy.gif"
-}
-let question3 = {
-    question: "Who is the main character in Air Force One?",
-    options: ["Harrison Ford", "Liam Neeson", "Keanu Reeves", "Mel Gibson"],
-    correct: "Harrison Ford",
-    gif: "https://media.giphy.com/media/3o7qDGJWZ9ql0OGcjC/giphy.gif"
-}
-let question4 = {
-    question: "Steven Seagal performed in: ",
-    options: ["The fugitive", "Under Siege", "Demolition man", "Blade"],
-    correct: "Under Siege",
-    gif: "https://media.giphy.com/media/O62idzzWt6wIU/giphy.gif"
-}
-let question5 = {
-    question: "In The Last Boy Scout Bruce Willis wroked for which president?",
-    options: ["Jimmy Carter", "Gerald Ford", "Ronald Reagan", "Richard Nixon"],
-    correct: "Jimmy Carter",
-    gif: "https://media.giphy.com/media/42zZWL1gWWnYQegojZ/giphy.gif"
-}
-let question6 = {
-    question: "In the movie Speed stars Keanu Reeves and? ",
-    options: ["Jackie Chan", "Chris Tucker", "Steven Seagal", "Sandra Bullock"],
-    correct: "Sandra Bullock"
-}
-let question7 = {
-    question: "Which year Assassins with Sylvester Stallone and Antonio Banderas was premier?",
-    options: ["1990", "1992", "1995", "1998"],
-    correct: "1995",
-    gif: "https://media.giphy.com/media/aq6Thivv9V9lu/giphy.gif"
-
-}
-let question8 = {
-    question: "Who stared Gremlins?",
-    options: ["Harrison Ford", "Zach Galligan", "Sean Young", "Rutger Hauer"],
-    correct: "Zach Galligan",
-    gif: "https://media.giphy.com/media/P0tIioXONXOz6/giphy.gif"
-}
-let question9 = {
-    question: "Which year Ghostbusters was premier?",
-    options: ["1984", "1985", "1983", "1986"],
-    correct: "1984",
-    gif: "https://media.giphy.com/media/26BoCdWySqRcaupWw/giphy.gif"
-}
-
 let randomQuestionArray = [];
 let randomAnswerArray = [];
 let questionNumber = 0;
@@ -89,16 +26,10 @@ $(document).ready(function () {
         randomQuestionArray = [];
         randomAnswerArray = [];
         questionNumber = 0;
-
         //console.log("limpiando campos");
-
     }
 
     function createStartButton() {
-        // let boton = $("<button>");
-        // boton.addClass("btn btn-primary");
-        // $(boton).text("Start");
-        // $(".boton").append(boton);
         $(".boton").show();
     }
 
@@ -135,12 +66,9 @@ $(document).ready(function () {
     }
 
     function responseTimerFun() {
-
         clearInterval(secondTimer);
         responseTimer = setTimeout(startGame, 3000);
-
     }
-
 
     function newQuestion() {
 
@@ -169,7 +97,6 @@ $(document).ready(function () {
             if (finish) {
                 clearInterval(secondTimer);
             }
-
         }
     }
 
@@ -182,8 +109,6 @@ $(document).ready(function () {
         if (!finish) {
             secondTimer = setTimeout(timer, 1000);
         }
-
-
     }
 
     function finishGame() {
@@ -193,7 +118,6 @@ $(document).ready(function () {
         $(".timeRemaining").empty();
         $(".question").empty();
         $(".list").empty();
-
         let pa = $("<p>");
         $(pa).text("Correct: " + correct);
         $(".question").append(pa);
@@ -204,8 +128,6 @@ $(document).ready(function () {
         $(pc).text("Unanswered: " + unanswered)
         $(".question").append(pc)
         $(".restart").show();
-
-
     }
 
     clear();
@@ -231,7 +153,6 @@ $(document).ready(function () {
         //console.log("Correct : " + correct);
         //console.log("Incorrect : " + incorrect);
         //console.log("Unanswered : " + unanswered);
-
     });
 
 
@@ -250,20 +171,6 @@ $(document).ready(function () {
         //console.log("restart");
         clear();
         $(".boton").show();
-
-
-
     });
-
-
-
-
-
-
-
-
-
-
-
 
 });
---
+++ assets/javascript/questionsObject.js
@@ -0,0 +1,60 @@
+let question0 = {
+    question: "Which year Die Hard was premier?",
+    options: ["1986", "1987", "1988", "1989"],
+    correct: "1988",
+    gif: "https://media.giphy.com/media/AphxZxXiVklYQ/giphy.gif"
+}
+let question1 = {
+    question: "Who was the director of Leathal Weapon?",
+    options: ["Mel Gibson", "Danny Glover", "Richard Donner", "Traci Wolfe"],
+    correct: "Richard Donner",
+    gif: "https://media.giphy.com/media/3b1JW7LxfsAKs/giphy.gif"
+}
+let question2 = {
+    question: "In Point break who is Johnny Utah?",
+    options: ["Gary Busey", "Patrick Swayze", "John C. McGinley", "Keanu Reeves"],
+    correct: "Keanu Reeves",
+    gif: "https://media.giphy.com/media/aDYXQy3W8XFG8/giphy.gif"
+}
+let question3 = {
+    question: "Who is the main character in Air Force One?",
+    options: ["Harrison Ford", "Liam Neeson", "Keanu Reeves", "Mel Gibson"],
+    correct: "Harrison Ford",
+    gif: "https://media.giphy.com/media/3o7qDGJWZ9ql0OGcjC/giphy.gif"
+}
+let question4 = {
+    question: "Steven Seagal performed in: ",
+    options: ["The fugitive", "Under Siege", "Demolition man", "Blade"],
+    correct: "Under Siege",
+    gif: "https://media.giphy.com/media/O62idzzWt6wIU/giphy.gif"
+}
+let question5 = {
+    question: "In The Last Boy Scout Bruce Willis wroked for which president?",
+    options: ["Jimmy Carter", "Gerald Ford", "Ronald Reagan", "Richard Nixon"],
+    correct: "Jimmy Carter",
+    gif: "https://media.giphy.com/media/42zZWL1gWWnYQegojZ/giphy.gif"
+}
+let question6 = {
+    question: "In the movie Speed stars Keanu Reeves and? ",
+    options: ["Jackie Chan", "Chris Tucker", "Steven Seagal", "Sandra Bullock"],
+    correct: "Sandra Bullock"
+}
+let question7 = {
+    question: "Which year Assassins with Sylvester Stallone and Antonio Banderas was premier?",
+    options: ["1990", "1992", "1995", "1998"],
+    correct: "1995",
+    gif: "https://media.giphy.com/media/aq6Thivv9V9lu/giphy.gif"
+
+}
+let question8 = {
+    question: "Who stared Gremlins?",
+    options: ["Harrison Ford", "Zach Galligan", "Sean Young", "Rutger Hauer"],
+    correct: "Zach Galligan",
+    gif: "https://media.giphy.com/media/P0tIioXONXOz6/giphy.gif"
+}
+let question9 = {
+    question: "Which year Ghostbusters was premier?",
+    options: ["1984", "1985", "1983", "1986"],
+    correct: "1984",
+    gif: "https://media.giphy.com/media/26BoCdWySqRcaupWw/giphy.gif"
+}
